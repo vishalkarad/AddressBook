ae642dd264d3a644f42a8407a0474d4355861bd3
vishalkarad <vishalkarad777@gmail.com>
2020-04-30 01:58:50 +0530

    ADDED : add exception class

--- src/main/java/com/bridgelabz/exception/AddressBookException.java
+++ src/main/java/com/bridgelabz/exception/AddressBookException.java
@@ -1,4 +1,14 @@
 package com.bridgelabz.exception;
 
-public class AddressBookException {
+public class AddressBookException extends Exception{
+
+    public enum MyException_Type{
+        WRONG_CHOICE,FILE_ALREADY_EXISTS;
+    }
+    MyException_Type type;
+
+    public AddressBookException(MyException_Type type,String message) {
+        super(message);
+        this.type = type;
+    }
 }
