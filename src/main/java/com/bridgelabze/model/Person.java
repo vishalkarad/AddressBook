8aa349807e82343630dc8a31a0667af42e87846c
vishalkarad <vishalkarad777@gmail.com>
2020-04-29 17:09:20 +0530

    ADDED : create person pojo class

---
+++ src/main/java/com/bridgelabze/model/Person.java
@@ -0,0 +1,18 @@
+package com.bridgelabze.model;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String state;
+    private String zip;
+
+    public Person(String firstName, String lastName, String city, String state, String zip) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+
+    }
+}
