3f41ba1cff0eccaa50e30f3f0a97a3cd2f48d79c
vishalkarad <vishalkarad777@gmail.com>
2020-04-30 14:27:42 +0530

    ADDED : add test case to add record in json file

--- src/test/java/AddressBookTest.java
+++ src/test/java/AddressBookTest.java
@@ -1,18 +1,34 @@
+import com.bridgelabz.exception.AddressBookException;
 import com.bridgelabz.model.Person;
 import com.bridgelabz.service.AddressBookMain;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import java.io.IOException;
 
 public class AddressBookTest {
 
-    AddressBookMain main = new AddressBookMain();
+    // Object reference
+    AddressBookMain main;
+
+    // constant variable
     private final String FILE_PATH = "F:\\bridgelabze\\addressBook\\addressBook\\myfile.json";
     public final String CREATE_FILE = "addressBook";
+    public final String ADD_RECORD = "addRecord";
+
+    @Before
+    public void setUp() {
+        main = new AddressBookMain();
+    }
 
     @Test
-    public void givenFilePath_WhenCreateNewFile_ThenReturnSuccessMessage() throws IOException {
+    public void givenFilePath_WhenCreateNewFile_ThenReturnSuccessMessage() throws AddressBookException {
         String result = main.addressBook(new Person(), FILE_PATH, CREATE_FILE);
         Assert.assertEquals("File Created Successfully", result);
+    }
+
+    @Test
+    public void givenFilePath_WhenAddPersonRecord_ThenReturnSuccessMessage() throws AddressBookException {
+        String result = main.addressBook(new Person("vivek", "karad", "shirur", "maharashtra", "431128"), FILE_PATH, ADD_RECORD);
+        Assert.assertEquals("Add Records Successfully", result);
     }
 }
