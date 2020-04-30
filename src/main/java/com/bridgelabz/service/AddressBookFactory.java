c7d7572104331bdd5756784bb2e2443790d64cb4
vishalkarad <vishalkarad777@gmail.com>
2020-04-30 14:41:16 +0530

    ADDED : add function to createAddressBookInterface to return AddressBook object

--- src/main/java/com/bridgelabz/service/AddressBookFactory.java
+++ src/main/java/com/bridgelabz/service/AddressBookFactory.java
@@ -1,12 +1,17 @@
 package com.bridgelabz.service;
-
 import com.bridgelabz.controller.AddressBookController;
 import com.bridgelabz.interfaces.AddressBookControllerInterface;
+import com.bridgelabz.interfaces.AddressBookInterface;
 
 public class AddressBookFactory {
 
     // Return Object Of AddressBookController
-    public static AddressBookControllerInterface createControlerInterface() {
+    public static AddressBookControllerInterface createControllerInterface() {
         return new AddressBookController();
+    }
+
+    // Return Object Of AddressBookController
+    public static AddressBookInterface createAddressBookInterface() {
+        return new AddressBook();
     }
 }
