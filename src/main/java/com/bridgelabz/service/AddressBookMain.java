087f6936f28ac2071649c1cd67da8b9286a123e3
vishalkarad <vishalkarad777@gmail.com>
2020-04-30 14:35:44 +0530

    MODIFIED : call add function to add person record in json file

--- src/main/java/com/bridgelabz/service/AddressBookMain.java
+++ src/main/java/com/bridgelabz/service/AddressBookMain.java
@@ -1,17 +1,30 @@
 package com.bridgelabz.service;
+
 import com.bridgelabz.exception.AddressBookException;
 import com.bridgelabz.interfaces.AddressBookControllerInterface;
+import com.bridgelabz.interfaces.AddressBookInterface;
 import com.bridgelabz.model.Person;
-
+import org.junit.Before;
 
 public class AddressBookMain {
 
+    // Object reference
+    AddressBookControllerInterface controller;
+    AddressBookInterface addressBook;
+
+    @Before
+    public void setUp() {
+        controller = AddressBookFactory.createControlerInterface();
+        addressBook = AddressBookFactory.createAddressBookInterface();
+    }
+
     // function address Book to provide service.
-    public String addressBook(Person person, String file_path, String create_file) throws AddressBookException {
-        AddressBookControllerInterface controller = AddressBookFactory.createControlerInterface();
-        switch (create_file) {
+    public String addressBook(Person person, String file_path, String choice) throws AddressBookException {
+        switch (choice) {
             case "addressBook":
                 return controller.createNewAddressBook(file_path);
+            case "addRecord":
+                return addressBook.addPersonRecord(person, file_path);
             default: {
                 throw new AddressBookException(AddressBookException.MyException_Type.WRONG_CHOICE, "Enter Right Choice");
             }
