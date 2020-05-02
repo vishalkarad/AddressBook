5e3d4ade527b1a1949cb3a9f4881c23005770ae4
vishalkarad <vishalkarad777@gmail.com>
2020-05-02 22:39:19 +0530

    ADDED : add two function to read and display the list of address book file

--- src/main/java/com/bridgelabz/service/FileMenuOptions.java
+++ src/main/java/com/bridgelabz/service/FileMenuOptions.java
@@ -1,4 +1,23 @@
 package com.bridgelabz.service;
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileMenuOptions {
+
+    // Display the all address book
+    public List<String> displayTheContentsOfADirectory(){
+        String filePath ="F:\\bridgelabze\\addressBook\\addressBook";
+        File directory = new File(filePath);
+        File[] fileOfDirectory = directory.listFiles();
+        return Arrays.stream(fileOfDirectory).map(k -> k.getName()).collect(Collectors.toList());
+    }
+
+    // Print all address book file
+    public void printContaintListInDirectory(){
+        List list = displayTheContentsOfADirectory();
+        System.out.println("List Of Files");
+        list.stream().forEach(k -> System.out.println(k));
+    }
 }
