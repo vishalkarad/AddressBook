0b38966739326bfe5b82e0aae9a88f9460aa9c82
vishalkarad <vishalkarad777@gmail.com>
2020-05-03 11:41:48 +0530

    ADDED : add getter methods

--- src/main/java/com/bridgelabz/model/Person.java
+++ src/main/java/com/bridgelabz/model/Person.java
@@ -1,4 +1,51 @@
 package com.bridgelabz.model;
 
 public class Person {
+
+    // Declare variable
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNumber;
+
+    // Default constructor
+    public Person() {
+    }
+
+    // Parameterized constructor
+    public Person(String firstName, String lastName, String city, String state, String zip, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Getter methods
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 }
