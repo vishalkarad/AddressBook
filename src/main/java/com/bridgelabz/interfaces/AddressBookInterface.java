b2fcc50aa1ec6cbf78b13f736d8a836a3ae3361d
vishalkarad <vishalkarad777@gmail.com>
2020-05-03 11:42:40 +0530

    ADDED : add interface methods

--- src/main/java/com/bridgelabz/interfaces/AddressBookInterface.java
+++ src/main/java/com/bridgelabz/interfaces/AddressBookInterface.java
@@ -1,7 +1,12 @@
 package com.bridgelabz.interfaces;
-
 import com.bridgelabz.model.Person;
+import java.io.IOException;
 
 public interface AddressBookInterface {
+
     public String addPersonRecord(Person person, String file_path);
+    public String deleteRecord(String... strings) throws IOException;
+    public String  sortListByName(String file_path) throws IOException;
+    public String sortListByZip(String file_path) throws IOException;
+    public void printEntries(String file_path) throws IOException;
 }
