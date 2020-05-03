6a91e7c96d594bac5cc84dc1a4650781e1a3f606
vishalkarad <vishalkarad777@gmail.com>
2020-05-03 11:40:54 +0530

    ADDED : add new interface and declare createNewAddressBook method

--- src/main/java/com/bridgelabz/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/controller/AddressBookController.java
@@ -1,22 +1,22 @@
-package com.bridgelabze.controller;
-
-import com.bridgelabze.service.AddressBookControllerInterface;
-
+package com.bridgelabz.controller;
+import com.bridgelabz.exception.AddressBookException;
+import com.bridgelabz.interfaces.AddressBookControllerInterface;
 import java.io.File;
 
 public class AddressBookController implements AddressBookControllerInterface {
 
+    // Creating a new address book
     @Override
     public String createNewAddressBook(String fileName) {
         try {
             File file = new File(fileName);
-            if (file.exists() == true)
-                return "File already exists";
-            else file.createNewFile();
+            if (file.exists() == true) {
+                throw new AddressBookException(AddressBookException.MyException_Type.FILE_ALREADY_EXISTS, "This File Already Exists");
+            }else file.createNewFile();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return "File Create Successfully";
+        return "File Created Successfully";
     }
 
 }
